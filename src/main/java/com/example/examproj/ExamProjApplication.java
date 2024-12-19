1bce1e740fb966c70142c0d13c9189091b3e4481
AmirbekAI <amirbek.islomov_2027@ucentralasia.org>
2024-12-19 20:18:47 +0600

    Initial commit

---
+++ src/main/java/com/example/examproj/ExamProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.examproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/examproj/ExamProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
